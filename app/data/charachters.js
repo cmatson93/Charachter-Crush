6b83e34922e0946656b799bcba77f016398002e3
Christina  Matson <christinamatson@christinas-mbp.attlocal.net>
2017-11-16 20:42:53 -0800

    Some css additions

--- app/data/charachters.js
+++ app/data/charachters.js
@@ -2,8 +2,8 @@
 
 var charactersArray = [
   {
-    "name":"Mario",
-    "photo":"https://supermariorun.com/assets/img/stage/mario03.png",
+    "name":"Jamie Lannister",
+    "photo":"https://upload.wikimedia.org/wikipedia/en/b/b4/Jaime_Lannister-Nikolaj_Coster-Waldau.jpg",
     "scores":[
         5,
         5,
@@ -13,8 +13,8 @@ var charactersArray = [
       ]
   },
   {
-    "name":"Princess Peach",
-    "photo":"https://vignette.wikia.nocookie.net/deathbattlefanon/images/d/de/Princess_Peach_%28Fortune_Street%29.png/revision/latest?cb=20140720001721",
+    "name":"Jon Snow",
+    "photo":"https://upload.wikimedia.org/wikipedia/en/thumb/f/f0/Jon_Snow-Kit_Harington.jpg/220px-Jon_Snow-Kit_Harington.jpg",
     "scores":[
         4,
         4,
@@ -24,8 +24,8 @@ var charactersArray = [
       ]
   },
   {
-    "name":"Zelda",
-    "photo":"http://www.zelda.com/assets/images/centerstage/breath-of-the-wild/icon.png",
+    "name":"Robb Stark",
+    "photo":"https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FRobb_Stark&psig=AOvVaw2xRgvJ9c_jei8NOXS9UEM7&ust=1510977097940306",
     "scores":[
         3,
         3,
@@ -35,7 +35,7 @@ var charactersArray = [
       ]
   },
   {
-    "name":"Twilight Princess",
+    "name":"Joffery Baratheon",
     "photo":"http://www.thinkgeek.com/images/products/zoom/jonk_twilight_princess_zelda_figma_fig.jpg",
     "scores":[
         2,
@@ -44,7 +44,51 @@ var charactersArray = [
         2,
         2
       ]
-  }
+  },
+  {
+    "name":"Tyrion Lannister",
+    "photo":"https://upload.wikimedia.org/wikipedia/en/thumb/5/50/Tyrion_Lannister-Peter_Dinklage.jpg/220px-Tyrion_Lannister-Peter_Dinklage.jpg",
+    "scores":[
+        3,
+        3,
+        3,
+        3,
+        3
+      ]
+  },
+  {
+    "name":"The Hound",
+    "photo":"https://upload.wikimedia.org/wikipedia/en/b/b4/Sandor_Clegane-The_Hound-Rory_McCann.jpg",
+    "scores":[
+        3,
+        3,
+        3,
+        3,
+        3
+      ]
+  },
+  {
+    "name":"Khal Drago",
+    "photo":"https://upload.wikimedia.org/wikipedia/en/e/ef/Khal_Drogo-Jason_Momoa.jpg",
+    "scores":[
+        3,
+        3,
+        3,
+        3,
+        3
+      ]
+  },
+  {
+    "name":"Samwell Tarly",
+    "photo":"https://vignette.wikia.nocookie.net/gameofthrones/images/e/e9/Sam_at_Winterfell.jpg/revision/latest?cb=20170903200558",
+    "scores":[
+        3,
+        3,
+        3,
+        3,
+        3
+      ]
+  },
 ];
 
 
