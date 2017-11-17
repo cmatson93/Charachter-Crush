04ac1b0132402b7446c836b14dce372363e81c72
Christina  Matson <christinamatson@christinas-mbp.attlocal.net>
2017-11-17 00:25:50 -0800

    Final Fixes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -42,32 +42,41 @@ module.exports = function(app) {
 	  var lowestDiff = 1000;
 	  for (var i = 0; i < charactersArray.length; i++) {
 	  	var totalDifference = 0;
+	  	console.log("*************");
 	  	 console.log(charactersArray[i].scores);
 	  	 for (var s = 0; s < charactersArray[i].scores.length; s++) {
+	  	 	console.log("--------");
+	  	 	// console.log("lowestDiff: ", lowestDiff);	
+	  	 	
 	  	 	if (charactersArray[i].scores[s] > scores[s]) {
 	  	 		var difference = charactersArray[i].scores[s] - scores[s];
 	  	 		totalDifference += difference;
-	  	 		if (totalDifference < lowestDiff) {
-	  	 			lowestDiff = totalDifference;
-	  	 			matched = charactersArray[i];
-	  	 		}
+	  	 		// if (totalDifference < lowestDiff) {
+	  	 		// 	lowestDiff = totalDifference;
+	  	 		// 	matched = charactersArray[i];
+	  	 		// }
 	  	 	}
 	  	 	else if (charactersArray[i].scores[s] < scores[s]) {
 	  	 		difference = scores[s] - charactersArray[i].scores[s];
 	  	 		totalDifference += difference;
-	  	 		if (totalDifference < lowestDiff) {
-	  	 			lowestDiff = totalDifference;
-	  	 			matched = charactersArray[i];
-	  	 		}
+	  	 		// if (totalDifference < lowestDiff) {
+	  	 		// 	lowestDiff = totalDifference;
+	  	 		// 	matched = charactersArray[i];
+	  	 		// }
 	  	 	}
 	  	 	else if (charactersArray[i].scores[s] === scores[s]){
-	  	 		difference = 0;
-	  	 		totalDifference = totalDifference + difference;
-	  	 		if (totalDifference < lowestDiff) {
-	  	 			lowestDiff = totalDifference;
-	  	 			matched = charactersArray[i];
-	  	 		}
+	  	 		// if (totalDifference < lowestDiff) {
+	  	 		// 	lowestDiff = totalDifference;
+	  	 		// 	matched = charactersArray[i];
+	  	 		// }
 	  	 	}
+	  	 }
+	  	 console.log("i: ", i);
+	  	 if (totalDifference < lowestDiff) {
+	  	 	lowestDiff = totalDifference;
+	  	 	console.log("totalDifference: ", totalDifference);
+	  	 	console.log("lowestDiff: ", lowestDiff);
+	  	 	matched = charactersArray[i];
 	  	 }
 	  }
 	  console.log("lowestDiff: ", lowestDiff);	
